4ac22e5fadd12d31764099dd777eb0c2339840ea
mozochek <kirill.fockin2012@yandex.ru>
2020-10-11 12:04:20 +0300

    Disable sleep mode of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'screens/home/home_screen.dart';
+import 'package:screen/screen.dart';
 
 void main() => runApp(Application());
 
 class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+  // Prevent screen from going into sleep mode:
+    Screen.keepOn(true);
     return MaterialApp(
       title: 'Прайс лист СЦ"Скала"',
       debugShowCheckedModeBanner: false,
